49827e85dbd48869bad6a8f8ebfe239d0727ae75
jeremysandor <jeremy.r.sandor@gmail.com>
2018-06-20 21:42:16 -0700

    jwt verification

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,20 @@
+const CognitoExpress = require("cognito-express");
+const cognitoExpress = new CognitoExpress({
+  region: "us-west-2",
+  cognitoUserPoolId: "us-west-2_zJjzLQOwE",
+  tokenUse: "access", //Possible Values: access | id
+  tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
+});
+
+
 exports.authenticate = async (req, res, next) => {
-  console.log('authenticate req', req);
-  console.log('authenticate res', res);
+  console.log('authenticate req', req.headers);
+  const accessTokenFromClient = req.headers['access-token'];
+  cognitoExpress.validate(accessTokenFromClient, function(err, response) { 
+    console.log('err', err)
+    console.log('response', response)
+  });
+  next();
 }
 
 exports.logRequest = (req, res, next) => {
